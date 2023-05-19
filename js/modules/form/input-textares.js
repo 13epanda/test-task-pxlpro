f75007787198d0acb4d32bb0c9fb3f9094c68924
13epanda <veratihonova95@gmail.com>
2023-05-19 14:00:49 +0500

    fix(input-textarea)

--- js/modules/form/input-textares.js
+++ js/modules/form/input-textares.js
@@ -1,15 +1,23 @@
+import { debounce } from "../../utils/debounce.js";
+
 const textarea = document.getElementsByTagName('textarea');
 
 const resizeTextarea = (el) => {
-    setTimeout(function() {
+    const heightStyle = el.target.style.height.replace('px', '');
+    if (heightStyle > el.target.scrollHeight) {
+        el.target.style.height = `${el.target.scrollHeight - 24}px`;
+    }
+    else {
         el.target.style.height = `${el.target.scrollHeight}px`;
-    }, 1);
+    }
 }
 
 const initTextArea = () => {
     if (textarea.length >= 1) {
         [].forEach.call(textarea, el => {
-            el.addEventListener('keydown', resizeTextarea);
+            el.addEventListener('keydown', function (el) {
+                debounce(resizeTextarea(el), 500);
+            });
         });
     }
 }
